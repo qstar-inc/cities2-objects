8c5f42606dc3ae0d7683520b38c7c145f169aaae
Qoushik <qstar.inc@gmail.com>
2024-04-05 17:29:26 +0600

    v2.01.2 minor update

--- util.js
+++ util.js
@@ -22,3 +22,11 @@ function formatDate(dateString) {
     month: "short",
   })} ${year}`;
 }
+
+String.prototype.toSentenceCase = function () {
+  var upperCaseNotAvailable = "0123456789!'?^|[]@#-.,"; //! And other...
+  if (upperCaseNotAvailable.indexOf(this[0]) < 0)
+    return this[0].toUpperCase() + this.slice(1);
+
+  return this;
+};
